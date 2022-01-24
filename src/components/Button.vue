481153f9668e9ca68bf1de5da66338b98e864d92
unknown <alextq3036@gmail.com>
2022-01-23 23:46:49 -0500

    Passed text and colr to button component

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+    },
+    color: {
+      type: String,
+      default: "black",
+    },
+  },
+};
+</script>
